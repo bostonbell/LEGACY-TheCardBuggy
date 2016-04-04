bbcd52e706edddb80e4daaeeb1db6d6b8dddcf75
Austin Fell <austinrf2918@yahoo.com>
2016-04-04 03:31:56 -0400

    Made multiple stylistic improvements

---
+++ css/makeme.py
@@ -0,0 +1,8 @@
+import os
+compile1 = 'sass shopHomepageBuild.scss shop-homepage.css'
+compile2 = 'sass homeBuild.scss home.css'
+compile3 = 'sass aboutBuild.scss about.css'
+
+os.system(compile1)
+os.system(compile2)
+os.system(compile3)
