da4d4857e5e4508eab110b3072c39dd054ed101f
Austin Fell <austinrf2918@yahoo.com>
2016-04-05 23:04:16 -0400

    Removed old scss files.

--- css/makeme.py
+++
@@ -1,8 +0,0 @@
-import os
-compile1 = 'sass shopHomepageBuild.scss shop-homepage.css'
-compile2 = 'sass homeBuild.scss home.css'
-compile3 = 'sass aboutBuild.scss about.css'
-
-os.system(compile1)
-os.system(compile2)
-os.system(compile3)
